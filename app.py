5eee1c3aec5a051de7aaa39c41369de98007cde8
kaykim0310 <kim.kangyoon@gmail.com>
2025-07-20 12:20:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # GitHub에서 복사했던 HTML 파일의 'Raw' URL을 여기에 넣으세요.
 # 'SmartTBM' 리포지토리에 올리신 HTML 파일의 Raw URL이어야 합니다.
-html_url = "https://raw.githubusercontent.com/kaykim0310/SmartTBM/refs/heads/main/smartTBMwithWORKTALK.html" 
+html_url = "https://raw.githubusercontent.com/kaykim0310/Smart-TBM/main/Smart-TBM.html" 
 
 # Streamlit 앱 제목 설정
 st.title("GitHub HTML 파일 표시하기")
